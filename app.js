616124b4aea7687d4b25cb6cabaf17f53c76b94e
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-08-07 00:52:27 +0530

    Updated App.js for MovieNames route using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const MovieNamesRoute = require('./routes/MovieNames');
+app.use('/MovieNames', MovieNamesRoute);
 
 const conn = require('./middlewares/connection');
 console.log("Trying to establish a connection to TiDB...");
